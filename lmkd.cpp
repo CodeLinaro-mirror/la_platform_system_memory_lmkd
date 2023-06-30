1a21dc9699091fa61a86ddb4916b5e15daa48908
Sahitya Puligadda <quic_spuligad@quicinc.com>
2023-06-30 21:35:39 +0530

    Fixed lmkd log printing for 1G memory
    Change-Id: I2bc5f511a3e8eeda56319d7f46a1c0e0473e68b4
    (cherry picked from commit 95838a253f1278c7331d9b9f5e8f8dd375166cb2)

--- lmkd.cpp
+++ lmkd.cpp
@@ -4161,8 +4161,9 @@ do_kill:
             } else {
                 min_score_adj = zone_watermarks_ok(level);
                 if (min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
-			if (debug_process_killing)
-				ULMK_LOG(I, "Ignoring pressure since per-zone watermarks ok");
+                    if (debug_process_killing) {
+                        ULMK_LOG(I, "Ignoring pressure since per-zone watermarks ok");
+                    }
                     return;
                 }
             }
